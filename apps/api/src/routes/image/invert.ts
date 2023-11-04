2b3e1f61392430d88e2f3690b7ef3203a52026f0
simxnet <simxnet@envs.net>
2023-11-04 14:40:53 +0100

    refactor: invert route

--- apps/api/src/routes/image/invert.ts
+++ apps/api/src/routes/image/invert.ts
@@ -10,25 +10,34 @@ const invert: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
 	fastify.post<{ Headers: ImageHeaders }>(
 		"/invert",
 		async function (request, reply) {
-			const rawImage = (await fetch(request.headers.image_url)
-				.then((x) => x.arrayBuffer())
-				.then((i) => decode(Buffer.from(i)))) as Image;
+			try {
+				const response = await fetch(request.headers.image_url);
+				if (!response.ok) {
+					reply.code(400).send("Failed to fetch the image");
+					return;
+				}
 
-			rawImage.invert();
-			rawImage.resize(rawImage.width - 50, rawImage.height - 50);
+				const buffer = await response.arrayBuffer();
+				const rawImage = (await decode(Buffer.from(buffer))) as Image;
 
-			const result =
-				request.headers.response_type === "jpeg"
-					? await rawImage.encodeJPEG(5)
-					: await rawImage.encode();
+				rawImage.invert();
+				rawImage.resize(rawImage.width - 50, rawImage.height - 50);
 
-			reply.header(
-				"Content-Type",
-				`image/${request.headers.response_type ?? "png"}`,
-			);
-			reply.header("X-Response-Time", Math.round(reply.getResponseTime()));
-			reply.header("X-Original-Size", `${rawImage.width}x${rawImage.height}`);
-			reply.send(result);
+				const result =
+					request.headers.response_type === "jpeg"
+						? await rawImage.encodeJPEG(5)
+						: await rawImage.encode();
+
+				reply.headers({
+					"Content-Type": `image/${request.headers.response_type ?? "png"}`,
+					"X-Response-Time": Math.round(reply.getResponseTime()).toString(),
+					"X-Original-Size": `${rawImage.width}x${rawImage.height}`,
+				});
+
+				reply.send(result);
+			} catch (error) {
+				reply.code(500).send("Internal Server Error");
+			}
 		},
 	);
 };
