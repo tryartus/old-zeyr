b7763c163af74202384f59c69043b232d0dd5747
simxnet <simxnet@envs.net>
2023-11-18 11:42:36 +0100

    feat(api): saucenao implementation

---
+++ apps/api/src/routes/sauce/find.ts
@@ -0,0 +1,29 @@
+import { FastifyPluginAsync } from "fastify";
+import sagiwi from "sagiri";
+
+const sauceClient = sagiwi(process.env.SAUCENAO_KEY!);
+
+interface SauceBody {
+	image_url?: string;
+	results?: number;
+}
+
+const find: FastifyPluginAsync = async (fastify, _): Promise<void> => {
+	fastify.post<{ Body: SauceBody }>("/find", async function (request, reply) {
+		if (!request.body.image_url) {
+			return reply.badRequest("invalid image url provided");
+		}
+
+		const sauce = await sauceClient(request.body.image_url, {
+			results: request.body.results ?? 10,
+		});
+
+		console.log(sauce);
+
+		return reply.send({
+			results: sauce,
+		});
+	});
+};
+
+export default find;
