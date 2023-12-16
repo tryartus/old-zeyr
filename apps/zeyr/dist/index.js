0b43d6da08b7079e84c60e6166c9bc43b70f47a9
simxnet <simxnet@envs.net>
2023-12-16 10:10:52 +0100

    feat: some corrections

--- apps/zeyr/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("#lib/client");
-const options_1 = require("#lib/options");
-require("#lib/setup");
-const client = new client_1.ZeyrClient({
-    context: options_1.ZeyrContext
-});
-void client.run();
--- apps/zeyr/dist/lib/setup.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const env_utilities_1 = require("@skyra/env-utilities");
-(0, env_utilities_1.setup)();
