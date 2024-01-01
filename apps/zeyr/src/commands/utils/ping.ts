d906fcbf4fa801aa0245722aafee8b86dde7c962
simxnet <simxnet@envs.net>
2024-01-01 15:33:02 +0100

    ci(biome): stfu biome

--- apps/zeyr/src/commands/utils/ping.ts
+++ apps/zeyr/src/commands/utils/ping.ts
@@ -11,8 +11,9 @@ export default class Command extends SubCommand {
 		const apiPing = await this.getAPILatency(ctx);
 
 		return ctx.editResponse({
-			content: `🌏 Discord latency: ${await this.getDiscordLatency(ctx)}ms${apiPing !== 0 ? `\n📡 Zeyr API latency: ${apiPing}ms` : ""
-				}`,
+			content: `🌏 Discord latency: ${await this.getDiscordLatency(ctx)}ms${
+				apiPing !== 0 ? `\n📡 Zeyr API latency: ${apiPing}ms` : ""
+			}`,
 		});
 	}
 
--- apps/zeyr/src/lib/common/util.ts
+++ apps/zeyr/src/lib/common/util.ts
@@ -4,35 +4,37 @@ import { minutes } from "./time";
 
 export const imageCache = new LimitedCollection({
 	expire: minutes(10),
-})
+});
 
 export async function returnBufferResponse(
 	ctx: ZeyrContext,
 	time: string | null,
 	data: ArrayBuffer,
 ) {
-	return !time ? ctx.editOrReply({
-		content: "this did not work"
-	}) : ctx.editOrReply(
-		{
-			content: "🖌️ done",
-			embeds: [
-				new MessageEmbed()
-					.setAuthor({
-						name: ctx.author.username,
-						iconUrl: ctx.author.avatarURL(),
-					})
-					.setFooter({
-						text: `${time}ms to complete`,
-					})
-					.setImage("attachment://result.png"),
-			],
-		},
-		[
-			{
-				data: Buffer.from(data),
-				name: "result.png",
-			},
-		],
-	);
+	return !time
+		? ctx.editOrReply({
+				content: "this did not work",
+		  })
+		: ctx.editOrReply(
+				{
+					content: "🖌️ done",
+					embeds: [
+						new MessageEmbed()
+							.setAuthor({
+								name: ctx.author.username,
+								iconUrl: ctx.author.avatarURL(),
+							})
+							.setFooter({
+								text: `${time}ms to complete`,
+							})
+							.setImage("attachment://result.png"),
+					],
+				},
+				[
+					{
+						data: Buffer.from(data),
+						name: "result.png",
+					},
+				],
+		  );
 }
